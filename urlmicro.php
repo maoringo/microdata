ec0aa0bde5d6b8af5b2d6594ca45e8975bc46b4b
Maori Ito <maori@nibio.go.jp>
2013-04-05 14:56:12 +0900

    First commit microdata file.

---
+++ urlmicro.php
@@ -0,0 +1,53 @@
+<?php
+include_once("Microdata.php");
+function parse_item(&$item){
+    $type = $item->type[0];
+    $type = substr(strrchr($type,"/"),1);
+   foreach($item->properties as $property => $values){
+        if($property == "taxon" or $property == "seeAlso"){//taxonまたはseeAlsoの場合にはNestに配慮
+            $nestproperty = $property;
+           foreach($values as $value){
+               if(is_object($value)){//Nest or not
+                   parse_item2($value,$nestproperty);
+                 }else{
+                    print "@".$type."_".$property."=".$value."\n";
+                 }
+           }
+        }
+       else{
+        foreach($values as $value){
+               if(is_object($value)){//Nest or not
+                   parse_item($value);
+                 }else{
+                    print "@".$type."_".$property."=".$value."\n";
+        }
+   } 
+}
+}
+}
+
+//In case of Nest (taxon or seeAlso) 
+function parse_item2(&$item,&$nestproperty){
+    $type = $item->type[0];
+    $type = substr(strrchr($type,"/"),1);
+   foreach($item->properties as $property => $values){
+       foreach($values as $value){
+               if(is_object($value)){//Nest or not
+                   parse_item2($value,$nestproperty);
+                 }else{
+                    print "@BiologicalDatabaseEntry_".$nestproperty."_".$type."_".$property."=".$value."\n";
+        }
+}
+}
+}
+
+$url = $argv[1];
+    echo $url."\n";
+$md = new Microdata($url); //note the class name is the same as original
+$data = $md->obj(); //$md->obj() if need PHP object
+foreach($data as $item){
+    parse_item($item);
+}
+
+
+?>
